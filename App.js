b6b74846a1255b381c4f641809305c0b2ac22ad4
Thaps23 <127627594+Thaps23@users.noreply.github.com>
2023-09-19 09:31:04 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator} from '@react-navigation/native';
+import { createStackNavigator} from '@react-navigation/native-stack';
 
 import LoginScreen from "./screens/Login";
 import SignupScreen from "./screens/SignUp";
